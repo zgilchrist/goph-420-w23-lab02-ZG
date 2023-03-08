6bec342bbd0805c485467fc83c8cc14286f2bf07
zgilchrist <72540268+zgilchrist@users.noreply.github.com>
2023-03-08 00:12:46 -0700

    functional root finder for two roots

--- driver.py
+++ driver.py
@@ -12,21 +12,23 @@ def main():
     beta1 = 1900
     beta2 = 3200
     H = 4000
+    f=1
 
-    love_fn = lambda x: rho2 / rho1 * np.sqrt(H**2 * (beta1**-2 - beta2**-2) - x**2) / x
+    love_fn = lambda x: rho2 / rho1 * np.sqrt(H**2 * (beta1**-2 - beta2**-2) - x**2) / x - np.tan(2*np.pi*f*x)
 
-    love_fn_prime = lambda x: rho2 / rho1 * -(H**2 * (beta1**-2 - beta2**-2)) / (x**2 * np.sqrt(H**2 * (beta1**-2 - beta2**-2) - x**2))
+    love_fn_prime = lambda x: rho2 / rho1 * -(H**2 * (beta1**-2 - beta2**-2)) / (x**2 * np.sqrt(H**2 * (beta1**-2 - beta2**-2) - x**2)) - (2*np.pi*f)*(1/np.cos(2*np.pi*f*x))**2
 
-    xk, k, eps_a_arr = rf.root_newton_raphson(1.5,love_fn,love_fn_prime)
+    xk, k, eps_a_arr = rf.root_newton_raphson(0.3,love_fn,love_fn_prime)
 
-    print(f"The value of the root is: {xk} and it took {k} iterations to converge.")
+    print(f"Newton Raphson: The value of the root is: {xk} and it took {k} iterations to converge.")
 
-    xk, k, eps_a_arr = rf.root_secant_modified(1,1.5,love_fn)
+    xk, k, eps_a_arr = rf.root_secant_modified(0.3,0.2,love_fn)
 
-    print(f"The value of the root is: {xk} and it took {k} iterations to converge.")
+    print(f"Modified Secant: The value of the root is: {xk} and it took {k} iterations to converge.")
 
-def love():
+def love(x,f):
     return None
+
 
 if __name__ == "__main__":
         main()
